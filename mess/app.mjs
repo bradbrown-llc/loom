4e7b1fec76548b179cb757f1fceccb2e1bac206f
TangleSpider <spider@tngl.io>
2022-05-24 20:07:11 -0400

    renaming, simplifying, standardizing

--- mess/app.mjs
+++ mess/app.mjs
@@ -1,35 +1,32 @@
 import k8s from '@kubernetes/client-node';
 import { InstancesClient } from '@google-cloud/compute';
-import { readFileSync } from 'fs';
 
 let project = process.env.PROJECT;
 let zone = process.env.ZONE;
+let ips = process.env.IPS;
+process.env.GOOGLE_APPLICATION_CREDENTIALS = '/etc/sec/gac';
 let kc = new k8s.KubeConfig();
 kc.loadFromDefault();
 let k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 let gc = new InstancesClient();
-let ips = getIps();
 
 async function main() {
     let instances = await getAllInstances();
     instances.forEach((instance, i) => fixInstanceIp(instance, ips[i]));
     k8sApi.patchNamespacedService(
-        "nginx-service", 
+        "nginx-svc", 
         "default",
-        { spec: { externalIPs: ips}},
-        undefined,
-        undefined,
-        undefined,
-        undefined,
+        { spec: { externalIPs: 
+            instances.map(instance =>
+                instance
+                    .networkInterfaces[0]
+                    .networkIP
+            )
+        }},
+        undefined, undefined, undefined, undefined,
         { headers: { 'content-type': 'application/merge-patch+json' }}
     )
 };
-
-function getIps() {
-    return readFileSync('/etc/cnf/ips', { encoding: 'utf8' })
-       .split(',')
-       .filter(ip => ip);
-} 
 
 async function getAllInstances() {
     let [ instances ] = await gc.list({ project: project, zone: zone });
@@ -43,7 +40,7 @@ function getInstanceIp(instance) {
         .natIP;
 }
 
-function fixInstanceIp(instance, ip) {
+async function fixInstanceIp(instance, ip) {
     if (ip && getInstanceIp(instance) == ip) return;
     if (!ip && ips.indexOf(getInstanceIp(instance)) == -1) return;
     await gc.deleteAccessConfig({
@@ -64,10 +61,7 @@ function fixInstanceIp(instance, ip) {
     await k8sApi.patchNode(
         instance.name, 
         { metadata: { labels: { 'messed-up': (!!ip).toString() }}},
-        undefined,
-        undefined,
-        undefined,
-        undefined,
+        undefined, undefined, undefined, undefined,
         { headers: { 'content-type': 'application/merge-patch+json' }}
     )
 }
